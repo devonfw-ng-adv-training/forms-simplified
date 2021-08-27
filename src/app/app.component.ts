6e8e1eabc7ff147f3449611ef42105bcb3d0ab1b
Timon Reineke <timon.reineke@capgemini.com>
2021-08-27 18:13:23 +0200

    Add validation and opt-out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   registrationForm: FormGroup;
+
+  private destroyed$ = new Subject();
 
   constructor(formBuilder: FormBuilder) {
     this.registrationForm = formBuilder.group({
       username: [null, Validators.required],
       password: [null, Validators.required],
-      rating: [null],
+      rating: [null, Validators.required],
+      isOptOut: [null],
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.registrationForm.controls.isOptOut.valueChanges
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((isOptOut: boolean) => {
+        if (isOptOut) {
+          this.registrationForm.controls.rating.reset();
+          this.registrationForm.controls.rating.disable();
+        } else {
+          this.registrationForm.controls.rating.enable();
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
 
   logDetails(): void {
     console.log(this.registrationForm.value);
